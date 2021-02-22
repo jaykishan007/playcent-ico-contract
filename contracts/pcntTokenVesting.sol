c5d29b56c2c1e7eeb75fdc8ffdd0f704e7164816
zaryab2000 <zaryabafser2000@gmail.com>
2021-02-22 20:22:50 +0530

    added tokenContract

--- contracts/pcntTokenVesting.sol
+++ contracts/pcntTokenVesting.sol
@@ -1,7 +1,24 @@
+pragma solidity 0.6.2;
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
 
-contract pcntTokenVesting {
-  constructor() public {
-  }
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+
+
+contract PlaycentToken is Initializable,OwnableUpgradeable,ERC20PausableUpgradeable{
+	using SafeMathUpgradeable for uint;
+
+	
+	function initialize() initializer public{
+		__Ownable_init();
+        __ERC20_init('Playcent','PCNT');		
+		__ERC20Pausable_init();
+		_mint(owner(),60000000 ether);
+ 		
+	}
+	
 }
